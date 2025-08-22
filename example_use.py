4dd06e376ce505c4a0e4702c3b464ea728de5a07
Chemandcode <113057775+Chemandcode@users.noreply.github.com>
2025-08-22 10:56:47 +0100

    Merging to main

--- example_use.py
+++ example_use.py
@@ -12,7 +12,7 @@ from asyncio import sleep
 if __name__ == "__main__":
 
     import asyncio
-    from ReactPyR_asyncua import ReactPyR
+    from ReactPyR import ReactPyR
 
     # Example with all the above functionality.
 
@@ -26,11 +26,11 @@ if __name__ == "__main__":
         # Starting an experiment.
         spectra_path = 'Digital Discovery Project\\asyncTest10'
         template_name = 'DigitalDiscoveryProject'
-<<<<<<< HEAD
-=======
         spectra_path = 'Digital Discovery Project\\asyncua_test10'
->>>>>>> nodeid_method
         await ir_machine.start_experiment(spectra_path, template_name, False)
+
+        # Retrieving the intensities of previous background spectra.
+        background = await ir_machine.get_last_background_spectra()
         await sleep(20)
         # await ir_machine.pause_experiment()
         # await ir_machine.get_current_sampling_interval()
@@ -42,15 +42,10 @@ if __name__ == "__main__":
         # await sleep(20)
         # await ir_machine.stop_experiment()
 
-<<<<<<< HEAD
         # # Retrieving the intensities of previous background spectra.
         # print("Retrieving last background spectra...")
         # background = await ir_machine.get_last_background_spectra()
         # print(background)
-=======
-        # Retrieving the intensities of previous background spectra.
-        background = await ir_machine.get_last_background_spectra()
->>>>>>> nodeid_method
 
         # # Collecting all the raw and processed IR spectra.
         await ir_machine.collect_raw_spectra()
